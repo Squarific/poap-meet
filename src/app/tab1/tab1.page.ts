50212dcbcb397e4df7d8564b157b99dad75dcaef
Filip Smets <filipsmetsbelgium@gmail.com>
2022-02-07 13:51:35 +0100

    Change dark theme and add http library

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+import { ElementRef } from '@angular/core';
+import { toCanvas as QRCodeToCanvas } from 'qrcode';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -8,8 +11,13 @@ import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 })
 export class Tab1Page {
   scanActive: boolean = false;
+  @ViewChild('qrtarget') qrtarget: ElementRef;
 
-  constructor() {}
+  publickey: string = "";
+
+  constructor(private http: HttpClient) {
+    this.publickey = localStorage.getItem("publickey");
+  }
 
   async checkPermission() {
     return new Promise(async (resolve, reject) => {
@@ -35,7 +43,10 @@ export class Tab1Page {
       if (result.hasContent) {
         this.scanActive = false;
         alert(result.content); //The QR content will come out here
-        //Handle the data as your heart desires here
+
+        this.http.post('http://localhost:3000/friends', { data: result.content }).subscribe((response) => {
+          console.log(response);
+        });
       } else {
         alert('NO DATA FOUND!');
       }
@@ -47,6 +58,15 @@ export class Tab1Page {
   stopScanner() {
     BarcodeScanner.stopScan();
     this.scanActive = false;
+  }
+
+  ionViewWillEnter() {
+    //if (this.publickey)
+      QRCodeToCanvas(this.qrtarget.nativeElement, this.publickey || "lkjfalkjfdlkaasdfasfsadfadsfasdfasdfasdjfldks");
+
+      this.http.post('http://localhost:3000/friends', { data: "fuck it" }).subscribe((response) => {
+          console.log(response);
+        });
   }
 
   ionViewWillLeave() {
